7a29c06fa12b0b4063f2171c8846a3dbe2ba8e36
Raghav Thakur <raghav@raghavs-macbook-pro.local>
2018-08-08 20:44:36 -0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Raghav Thakur on 2018-08-08.
+//  Copyright © 2018 Raghav Thakur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
